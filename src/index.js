7d3cb3ca0b2184962c0088208715dcf62e2d5f84
botshen <363088847@qq.com>
2021-06-12 09:59:10 +0800

    使用原本的useEffect

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import * as ReactDOM from "react-dom"
 import {createContext, useContext, useEffect, useState} from "react"
 
-
-
 function App() {
   const [n, setN] = useState(0)
   const onClick= ()=>{
     setN(i=>i+1)
   };
-  const afterRender = useEffect
-  afterRender(()=>{
+
+  useEffect(()=>{
     console.log("我第一次被渲染了")
   },[])
+  useEffect(()=>{
+    console.log("n变了我就执行")
+  },[n])
+  useEffect(()=>{
+    console.log("任何东西变化了我都执行")
+  })
   return (
    <div>
      n:{n}
